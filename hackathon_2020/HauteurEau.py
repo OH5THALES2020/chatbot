ce1cd870e4fec4ae6b5167aef8183dacdbec1944
Philippe DENGLOS <philippe.denglos@sfr.fr>
2020-10-10 12:57:17 +0200

    Ajout d'un module et d'une fonction de calcul de la hauteur d'eau en metres

---
+++ hackathon_2020/HauteurEau.py
@@ -0,0 +1,15 @@
+'''
+Created on 10 oct. 2020
+
+@author: philippe
+'''
+
+#focntion qui calcule la  hauteur d'eau
+# - a une position geographique donnee (latitude / longitude)
+# - a une date donnee
+# Unites :
+# latitude et longitude : degres
+# date UTC
+# resultat donne en metres
+def calculerHauteurDeau(latitude,longitude,date):
+        return 2
