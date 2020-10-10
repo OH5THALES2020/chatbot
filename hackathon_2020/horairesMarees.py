c1b43059785e7ed3f6e9f2446af02265c038e8b1
user <user@pc-ocean1.ocean>
2020-10-10 20:20:29 +0200

    horaire maree

--- hackathon_2020/horairesMarees.py
+++ hackathon_2020/horairesMarees.py
@@ -8,7 +8,7 @@ tidesDataFileLeHavre = "./hackathon_2020/data/annuaire_maree_LE_HAVRE_2020.csv"
 tidesDataFileSaintMalo = "./hackathon_2020/data/annuaire_maree_SAINT-MALO_2020.csv"
 tidesDataFileSocoa = "./hackathon_2020/data/annuaire_maree_SOCOA_2020.csv"
 
-def getMaree(ville, date, pm_bm):
+def getMaree(ville, date, pm_bm='pm'):
     print("REQUEST ville: ", ville, ", date: ", date, ", PM/BM: "+pm_bm)
     fileName = getDataFileName(ville)
     try:
@@ -19,15 +19,15 @@ def getMaree(ville, date, pm_bm):
          return readTidesFile(fileName, date, pm_bm)
 
 def getDataFileName(ville):
-    if ville is "boulogne-sur-mer":
+    if ville is 'boulogne-sur-mer':
         return tidesDataFileBoulogneSurMer
     if ville is "brest":
         return tidesDataFileBrest
-    if ville is "la rochelle":
+    if ville is "larochelle":
         return tidesDataFileLaRochelle
-    if ville is "le havre":
+    if ville is "lehavre":
         return tidesDataFileLeHavre
-    if ville is "saint-malo":
+    if ville is "saintmalo":
         return tidesDataFileSaintMalo
     if ville is "socoa":
         return tidesDataFileSocoa
@@ -54,4 +54,4 @@ def searchTide(requestDate, fileName, heureMatinHeader, heureSoirHeader):
             continue
 
 if __name__=="__main__":
-    print(getMaree("brest","10/10/2020","bm"))
+    print(getMaree('larochelle',"10/10/2020"))
