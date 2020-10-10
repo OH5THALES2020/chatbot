4a5bc16c3c13cd87c94e489a6889fa1a1bd220df
Mickaël Dréan <mickael.drean@gmail.com>
2020-10-10 11:50:45 +0200

    Premier essai de requete vers meteo france

---
+++ tests/api_meteofrance/main_essais_meteofrance.py
@@ -0,0 +1,35 @@
+from meteofrance.client import MeteoFranceClient
+from meteofrance.helpers import readeable_phenomenoms_dict
+
+# Init client
+client = MeteoFranceClient()
+
+# Search a location from name.
+city = "Brest"
+list_places = client.search_places(city)
+my_place = list_places[0]
+
+# Fetch weather forecast for the location
+my_place_weather_forecast = client.get_forecast_for_place(my_place)
+
+# Get the daily forecast
+my_place_daily_forecast = my_place_weather_forecast.daily_forecast
+
+# If rain in the hour forecast is available, get it.
+if my_place_weather_forecast.position["rain_product_available"] == 1:
+    my_place_rain_forecast = client.get_rain(my_place.latitude, my_place.longitude)
+    next_rain_dt = my_place_rain_forecast.next_rain_date_locale()
+    if not next_rain_dt:
+        rain_status = "No rain expected in the following hour."
+    else:
+        rain_status = next_rain_dt.strftime("%H:%M")
+else:
+    rain_status = "No rain forecast availble."
+
+print (rain_status)
+
+# Fetch weather alerts.
+my_place_weather_alerts = client.get_warning_current_phenomenoms(my_place.admin2)
+readable_warnings = readeable_phenomenoms_dict(
+    my_place_weather_alerts.phenomenons_max_colors
+)
