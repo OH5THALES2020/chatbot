40e6e9dba8be73ce893326dac706d8b0cf4f6561
Mickaël Dréan <mickael.drean@gmail.com>
2020-10-11 11:18:08 +0200

    Message meteo marine quand pas de bulletin cotier pour la ville donnée

--- hackathon_2020/condition_climat.py
+++ hackathon_2020/condition_climat.py
@@ -106,91 +106,90 @@ def getMeteoMarine(ville):
     print (descr)
 
     returnedStr = ""
-    if (bulletinCote == True):
+    if (bulletinCote == False):
+        returnedStr = "Je ne trouve pas de bulletin cotier pour '"+nomVille + "'. "
+    elif (bulletinCote == True):
         returnedStr = returnedStr + "Bulletin cotier de '"+nomVille + "', "
-    elif (bulletinMontagne == True):
-        returnedStr = returnedStr + "Bulletin montagne de '"+nomVille + "', "
-    else :
-        returnedStr = returnedStr + "Météo pour la ville de '"+nomVille + "', "
+    
+            
+        date = int(resume_today['date'])
+
+        #timestamp = 1602354456
+        #dt_object = datetime.fromtimestamp(timestamp)
+        #print("dt_object =", dt_object)
+
+        tutu = datetime.fromtimestamp((date/1000)) #gros hack...
+
+        heure = int(tutu.strftime("%H"))
+
+        if (bulletinCote == True):
+            if heure >= 22 :
+                returnedStr = returnedStr + " Nuit. "
+            elif heure >= 18:
+                returnedStr = returnedStr + " Soir. "
+            elif heure >= 12:
+                returnedStr = returnedStr + " Après-midi. "
+            else:
+                returnedStr = returnedStr + " Matin. "
+
+        dateStr = tutu.strftime("%A %e %B")
+        print (dateStr)
+        returnedStr = returnedStr + "Prévisions pour la journée du " + dateStr +" ."
         
-    date = int(resume_today['date'])
+        ventForce = int(resume_today['vitesseVent'])    
+        forceRafales = int(resume_today['forceRafales'])
+        directionVent = int(resume_today['directionVent'])
 
-    #timestamp = 1602354456
-    #dt_object = datetime.fromtimestamp(timestamp)
-    #print("dt_object =", dt_object)
+        if (directionVent > 315) :
+            directionVentStr = " Nord Nord Ouest"
+        elif (directionVent == 315) :
+            directionVentStr = " Nord Ouest"
+        elif (directionVent > 270) :
+            directionVentStr = " Nord Ouest"
+        elif (directionVent == 270) :
+            directionVentStr = " Ouest"
+        elif (directionVent > 225) :
+            directionVentStr = " Ouest Sud Ouest"
+        elif (directionVent == 225) :
+            directionVentStr = " Sud Ouest"
+        elif (directionVent > 180) :
+            directionVentStr = " Sud Sud Ouest"
+        elif (directionVent == 180) :
+            directionVentStr = " Sud"
+        elif (directionVent > 135) :
+            directionVentStr = " Sud Sud Est"
+        elif (directionVent == 135) :
+            directionVentStr = " Sud Est"
+        elif (directionVent > 90) :
+            directionVentStr = " Est Sud Est"
+        elif (directionVent == 90) :
+            directionVentStr = " Est"
+        elif (directionVent > 45) :
+            directionVentStr = " Est Nord Est"
+        elif (directionVent == 45) :
+            directionVentStr = " Nord Est"
+        elif (directionVent > 0 ) :
+            directionVentStr = " Nord Nord Est"
+        elif (directionVent == 0 ) :
+            directionVentStr = " Nord"
 
-    tutu = datetime.fromtimestamp((date/1000)) #gros hack...
+        returnedStr = returnedStr + " VENT, direction " + directionVentStr + " de " + str(ventForce) + " km/h "
+        if (forceRafales > 0) :
+            returnedStr = returnedStr + " avec des rafales à " + str(forceRafales) + " km/h."
+        
 
-    heure = int(tutu.strftime("%H"))
+        #" VENT : Nord-Ouest 4 à 5, fraichissant 5 à 6 en Manche l'après-midi.  #TODO
+        # MER : agitée. HOULE : Ouest à Nord-Ouest 2 m sur pointe Bretagne.  #TODO
+        # TEMPS : Ciel nuageux..
+        returnedStr = returnedStr + ". " + getConditionCielCourant(nomVille)
 
-    if (bulletinCote == True):
-        if heure >= 22 :
-            returnedStr = returnedStr + " Nuit. "
-        elif heure >= 18:
-            returnedStr = returnedStr + " Soir. "
-        elif heure >= 12:
-            returnedStr = returnedStr + " Après-midi. "
-        else:
-            returnedStr = returnedStr + " Matin. "
+        probaPluie = resume_today['probaPluie']
+        print (probaPluie)
+        
+        if (probaPluie is int and probaPluie > 0) :
+            returnedStr = returnedStr + " avec probabilité de pluie de " + str(probaPluie) + "%."
+        # VISIBILITE : Bonne."
 
-    dateStr = tutu.strftime("%A %e %B")
-    print (dateStr)
-    returnedStr = returnedStr + "Prévisions pour la journée du " + dateStr +" ."
-    
-    ventForce = int(resume_today['vitesseVent'])    
-    forceRafales = int(resume_today['forceRafales'])
-    directionVent = int(resume_today['directionVent'])
-
-    if (directionVent > 315) :
-        directionVentStr = " Nord Nord Ouest"
-    elif (directionVent == 315) :
-        directionVentStr = " Nord Ouest"
-    elif (directionVent > 270) :
-        directionVentStr = " Nord Ouest"
-    elif (directionVent == 270) :
-        directionVentStr = " Ouest"
-    elif (directionVent > 225) :
-        directionVentStr = " Ouest Sud Ouest"
-    elif (directionVent == 225) :
-        directionVentStr = " Sud Ouest"
-    elif (directionVent > 180) :
-        directionVentStr = " Sud Sud Ouest"
-    elif (directionVent == 180) :
-        directionVentStr = " Sud"
-    elif (directionVent > 135) :
-        directionVentStr = " Sud Sud Est"
-    elif (directionVent == 135) :
-        directionVentStr = " Sud Est"
-    elif (directionVent > 90) :
-        directionVentStr = " Est Sud Est"
-    elif (directionVent == 90) :
-        directionVentStr = " Est"
-    elif (directionVent > 45) :
-        directionVentStr = " Est Nord Est"
-    elif (directionVent == 45) :
-        directionVentStr = " Nord Est"
-    elif (directionVent > 0 ) :
-        directionVentStr = " Nord Nord Est"
-    elif (directionVent == 0 ) :
-        directionVentStr = " Nord"
-
-    returnedStr = returnedStr + " VENT, direction " + directionVentStr + " de " + str(ventForce) + " km/h "
-    if (forceRafales > 0) :
-        returnedStr = returnedStr + " avec des rafales à " + str(forceRafales) + " km/h."
-    
-
-    #" VENT : Nord-Ouest 4 à 5, fraichissant 5 à 6 en Manche l'après-midi.  #TODO
-    # MER : agitée. HOULE : Ouest à Nord-Ouest 2 m sur pointe Bretagne.  #TODO
-    # TEMPS : Ciel nuageux..
-    returnedStr = returnedStr + ". " + getConditionCielCourant(nomVille)
-
-    probaPluie = resume_today['probaPluie']
-    print (probaPluie)
-    
-    if (probaPluie is int and probaPluie > 0) :
-        returnedStr = returnedStr + " avec probabilité de pluie de " + str(probaPluie) + "%."
-    # VISIBILITE : Bonne."
-
-    
+        
     print (returnedStr)
     return returnedStr
